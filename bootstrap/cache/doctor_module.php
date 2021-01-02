0b97ca74dea70cb69c8d2c994e6e6b38cfbbade4
hagar-mohamed-reda <gogo202046@yahoo.com>
2021-01-02 11:47:10 +0200

    create doctor and admin modules

---
+++ bootstrap/cache/doctor_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Doctor\\Providers\\DoctorServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Doctor\\Providers\\DoctorServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
