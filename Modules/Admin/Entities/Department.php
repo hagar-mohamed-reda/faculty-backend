c41e451b32e894f3f7642498f9ddba28838282e5
Mohamed Salah <43927801+MohamedSalah17@users.noreply.github.com>
2021-01-03 13:53:22 +0200

    add department

--- Modules/Admin/Entities/Department.php
+++ Modules/Admin/Entities/Department.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
-    protected $fillable = [];
+    protected $table = 'departments';
+    protected $fillable = [
+        'division_id',
+        'level_id',
+        'faculty_id'
+    ];
+
+    protected $appends = ['can_delete'];
+
+    public function getCanDeleteAttribute() {
+        return true;
+    }
 }
