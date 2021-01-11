71f1979b0bdefe2beeabb65649110332990cc136
Mohamed Salah <43927801+MohamedSalah17@users.noreply.github.com>
2021-01-11 11:51:35 +0200

    use DB

--- Modules/Doctor/Http/Controllers/MainController.php
+++ Modules/Doctor/Http/Controllers/MainController.php
@@ -5,7 +5,7 @@ namespace Modules\Doctor\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
-
+use DB;
 class MainController extends Controller
 {
     public function getQuestionLevel(){
