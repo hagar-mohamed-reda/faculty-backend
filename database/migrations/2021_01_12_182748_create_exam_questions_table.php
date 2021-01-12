4804621551a5f77ada0465a3f9e49c57730d6527
Mohamed Salah <43927801+MohamedSalah17@users.noreply.github.com>
2021-01-12 20:31:23 +0200

    exam question migration

---
+++ database/migrations/2021_01_12_182748_create_exam_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExamQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exam_questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('exam_id');
+            $table->integer('question_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table("exam_questions", function ($table) {
+            $table->dropSoftDeletes();
+        });
+    }
+}
